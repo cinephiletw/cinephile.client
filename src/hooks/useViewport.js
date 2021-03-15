5c5f6385e7bff2c22a95f2ae7eebc3cf17101e46
蔡承諺 <ym851010@gmail.com>
2021-03-15 18:01:34 +0800

    fix: let hook 'useViewport.js' could detect the height of media

--- src/hooks/useViewport.js
+++ src/hooks/useViewport.js
@@ -2,14 +2,18 @@ import { React, useEffect, useState } from 'react';
 
 const useViewport = () => {
   const [mediaWidth, setMediaWidth] = useState(window.innerWidth);
+  const [mediaHeight, setMediaHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    const handleWindowResize = () => setMediaWidth(window.innerWidth);
+    const handleWindowResize = () => {
+      setMediaWidth(window.innerWidth);
+      setMediaHeight(window.innerHeight);
+    };
     window.addEventListener('resize', handleWindowResize);
     return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
 
-  return { mediaWidth };
+  return { mediaWidth, mediaHeight };
 };
 
 export default useViewport;
