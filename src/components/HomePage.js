af97be2dd75bfa6ecab47c49881c394c51973be6
蔡承諺 <ym851010@gmail.com>
2021-03-30 17:25:52 +0800

    fix: restruct populer with general js file

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
-import Popular from './Popular';
+import MovieBar from './MovieBar';
+import HomePageLabel from './HomePageLabel';
 import useViewport from '../hooks/useViewport';
 
 // 首頁，react 以component 為單位往下拆分，將元件寫在/component 資料夾內再export 出來使用
@@ -38,21 +39,6 @@ const HomePage = (props) => {
     },
   };
 
-  const popularLabel = {
-    base: {
-      fontFamily: 'sans-serif',
-      fontSize: '1.5em',
-      fontWeight: '700',
-      color: 'rgb(200, 200, 200)',
-      marginLeft: '5%',
-      height: '5px',
-      width: `${mediaWidth}`,
-    },
-    mobile: {
-      marginTop: '18px',
-    },
-  };
-
   return (
     <div className="layout" style={homeStyle}>
       <NavBar />
@@ -64,11 +50,9 @@ const HomePage = (props) => {
             : { ...homeMovieStyle.base, ...homeMovieStyle.laptop }
         }
       >
-        <h2 style={{ ...popularLabel.base, ...popularLabel.mobile }}>現正熱映中</h2>
-        <Popular positionV={1} />
-        <Popular positionV={2} />
-        <Popular positionV={3} />
-        <Popular positionV={4} />
+        <MovieBar positionV={1} movieBarType="hot" />
+        <MovieBar positionV={2} movieBarType="popular" />
+        <MovieBar positionV={3} movieBarType="coming" />
       </div>
     </div>
   );
